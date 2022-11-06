bf7542fe6dc2a34e4f962c0b0c9a193d59539210
Technical-Avenge <76620513+Avenge-hacker@users.noreply.github.com>
2022-11-06 07:31:24 -0800

    Add files via upload

---
+++ Core/Update/update.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+clear
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) v 2.0
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         █ 15% $rset"
+sleep 2.0
+clear
+echo -e "$red                         ██ 30% $rset"
+sleep 2.0
+clear
+echo -e "$red                         ███ 45% $rset"
+sleep 2.0
+clear
+echo -e "$red                         ████ 60% $rset"
+sleep 2.0
+clear
+echo -e "$red                         █████ 75% $rset"
+sleep 2.0
+clear
+echo -e "$red                         ███████ 99% $rset"
+sleep 2.0
+clear
+echo -e "$red                         ████████ 100% $rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                Updating the Instainsane please wait$rset"
+sleep 2.0
+clear
+cd $HOME
+cd /data/data/come.termux/files/home
+rm -rf Instainsane
+clear
+cd $HOME
+cd /data/data/come.termux/files/home
+git clone https://github.com/Avenge-hacker/Instainsane
+cd Instainsane
+chmod +x install.sh
+echo " "
+echo -e "$grn               Instainsane has been updating check it now...$rset"
+sleep 3.0
+echo " "
+./instainsane.sh
